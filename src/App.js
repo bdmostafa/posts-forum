bdc3e6cc726c1525f1b4fd6464a390b4b1b0aced
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-09-06 07:29:28 +0600

    project setup - materiul UI, react-router-dom and components created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  // Store the data
+  const [posts, setposts] = useState([]);
+
+  // Fetch the data
+  const loadData = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => setposts(data))
+  }
+
+  // useEffect hook executes when page is loaded
+  useEffect(loadData, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {posts && posts.map(post => post.title)}
     </div>
   );
 }
